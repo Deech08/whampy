f7cbc4c793bde2612a779610efa0f3308e7d0bc3
Dhanesh (DK) Krishnarao <31860356+Deech08@users.noreply.github.com>
2025-12-11 12:07:01 -0700

    version Updates
    Version updates and moving away from astropy_helpers (outdated)

--- whampy/__init__.py
+++ whampy/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import sys
 
-__minimum_python_version__ = "3.6"
+__minimum_python_version__ = "3.9"
 
 class UnsupportedPythonError(Exception):
     pass
